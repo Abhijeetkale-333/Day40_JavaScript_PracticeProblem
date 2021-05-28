bc3e562aa335dc0c1b9e4b5640915306aff6a557
Your Name <you@example.com>
2021-05-28 21:26:04 +0530

    Added: SwitchCase Practice Problems

--- WeekDay_Case_Statement.js
+++ WeekDay_Case_Statement.js
@@ -2,21 +2,32 @@ const prompt = require('prompt-sync')();
 let day = prompt('Enter number of day from the week= ');
 day = Number(day);
 
-if(day == 0)
-    console.log('Error! Not a valid week day');
-else if(day == 1)
+switch(day){
+    case 0:
+    console.log('Error! Invalid day');
+    break;
+  case 1:
     console.log('Monday');
-else if(day == 2)
+    break;
+  case 2:
     console.log('Tuesday');
-else if(day == 3)
+    break;
+  case 3:
     console.log('Wednesday');
-else if(day == 4)
+    break;
+  case 4:
     console.log('Thursday');
-else if(day == 5)
+    break;
+  case 5:
     console.log('Friday');
-else if(day == 6)
+    break;
+  case  6:
     console.log('Saturday');
-else if(day == 7)
+    break;
+  case  7:
     console.log('Sunday');
-else if(day >= 8)
-    console.log('Oops..!! Not a valid week day');
+    break;
+  default:
+    console.log('OOps..!!Please Enter correct weekday');
+    break;
+}
